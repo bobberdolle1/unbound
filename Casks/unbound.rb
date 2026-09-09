e4454c23646537a67844eafdda776b7881795c07
bobberdolle1 <kirillkrovyakov@yandex.com>
2026-09-09 12:42:23 +0300

    chore(cask): update release sha256 checksum in Casks/unbound.rb

--- Casks/unbound.rb
+++ Casks/unbound.rb
@@ -1,6 +1,6 @@
 cask "unbound" do
   version "0.6.7"
-  sha256 "b6a603b63d7c5dbc207761156c914f0ab8edef9917b8205250ac95a2aab39337"
+  sha256 "06b6ac34ef807b838c3a8ead53fe5909c4f0179af952178437c03f4d17e86e89"
 
   url "https://github.com/bobberdolle1/unbound/releases/download/v#{version}/unbound-v#{version}-macOS-Installer.pkg"
   name "UNBOUND"
